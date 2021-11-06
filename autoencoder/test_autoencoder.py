b2c153e08b156d36a71c41b950bbdef6b78c35a7
Felix Rittler <felix.rittler@outlook.com>
2021-11-06 15:13:11 +0100

    test autoencoder 13694de2d2424a379efaa48beb645d2dabcb4604

--- autoencoder/test_autoencoder.py
+++ autoencoder/test_autoencoder.py
@@ -6,10 +6,10 @@ from torch.utils.tensorboard import SummaryWriter
 import Autoencoder
 from torch_service import get_device, load_paths_from_folder, plot_classes_preds, show_torch_image
 
-DATASET_PATH = "datasets/validation_test"
+DATASET_PATH = "datasets/validation_set"
 
 # Autoencoder and training data
-autoencoder_path = "trained_autoencoders/run_13694de2d2424a379efaa48beb645d2dabcb4604.pt"
+autoencoder_path = "run_13694de2d2424a379efaa48beb645d2dabcb4604.pt"
 model = Autoencoder.AutoencoderVAEMediumConvSmallKernelBigBottleneck()
 BATCH_SIZE = 1
 
@@ -40,7 +40,7 @@ show_torch_image(images[0])
 criterion = nn.MSELoss()
 
 # push to available device
-device = 'cpu'
+device = get_device()
 print(device)
 model.to(device)
 
@@ -49,14 +49,11 @@ model.load_state_dict(torch.load(autoencoder_path, device))
 
 # monitor training loss
 test_loss = 0.0
-running_loss = 0.0
 i=0
 print(len(dataset_loader))
 
 # iterate over batches in dataset
 for data in dataset_loader:
-    print('Batch Index: {}'.format(i))
-    i+=1
     images = data
     images = images.to(device)
     # predict
@@ -71,14 +68,14 @@ for data in dataset_loader:
     )
 
     test_loss += loss.item()
-    running_loss += loss.item()
-    if i % log_rhythm == log_rhythm - 1:    # every n batches...
-        print("Current Loss: {}".format(running_loss / log_rhythm))
-        # ...log a figure showing the model's predictions
-        writer.add_figure('predictions',
-                        plot_classes_preds(model, images, outputs),
-                        i)
-        model.to(device)
-        running_loss = 0.0
+    if (i % 25 == 0):
+        print('Batch Index: {}'.format(i))
+        print("Current Loss: {}".format(loss.item()))
+    i+=1
 test_loss = test_loss/len(dataset_loader)
 print('Overall Average Loss: {:.6f}'.format(test_loss))
+writer.add_scalar(
+    'overall_loss',
+    test_loss,
+    0
+)
